0624174cc38039d0dbdbea8c4007e365ed258e89
amerre <adr.merre@gmail.com>
2019-02-01 12:30:01 +0100

    :construction: Modifying files structure

--- components/projects/Projects.vue
+++ components/projects/Projects.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="projects">
-    <div>
-      <nuxt-link to="/projects/project">
-        <sub>Day 2 - March 23, 2001</sub>
-        <h1>Learn to something</h1>
-      </nuxt-link>
-    </div>
-    <div>
-      <nuxt-link to="/projects/project">
-        <sub>Day 2 - March 23, 2001</sub>
-        <h1>Learn to something</h1>
-      </nuxt-link>
+  <div>
+    <div class="projects" v-for="days in $store.state" :key="days">
+      <button @click="days.active = !days.active">
+        <sub>{{ days.date }}</sub>
+        <h1>{{ days.title }}</h1>
+      </button>
+      <div class="projectContent" v-if="days.active">
+        <strong>What I did :</strong>
+        <p>{{ days.whatIDid }}</p>
+        <strong>Thoughts :</strong>
+        <p>{{ days.thoughts }}</p>
+      </div>
     </div>
   </div>
 </template>
+
---
+++ store/index.js
@@ -0,0 +1,28 @@
+import Vuex from "vuex";
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      "01": {
+        active: false,
+        title: "Learning to Sass",
+        date: "Day 45 - June 13, 2005",
+        whatIDid:
+          "Lorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsum.",
+        thoughts:
+          "Lorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem."
+      },
+      "02": {
+        active: false,
+        title: "Learning to talk",
+        date: "Day 2 - March 23, 2001",
+        whatIDid:
+          "Lorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsum.",
+        thoughts:
+          "Lorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem ipsumorem."
+      }
+    }
+  });
+};
+
+export default createStore;
