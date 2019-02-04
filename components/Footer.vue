42b633d3aadb9f9d0a381bd760d4dceebfb0cf5c
amerre <adr.merre@gmail.com>
2019-02-04 10:41:25 +0100

    :construction: Adding LinkedIn on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,9 @@
     <a href="https://github.com/amerre" target="_blank">
       <img src="../assets/images/github.svg">
     </a>
+    <a href="https://www.linkedin.com/in/adrienmerre/" target="_blank">
+      <img src="../assets/images/linkedin.svg">
+    </a>
   </div>
 </template>
 
--- components/home/AboutTheChallenge.vue
+++ components/home/AboutTheChallenge.vue
@@ -2,8 +2,10 @@
   <div>
     <h1>About this challenge</h1>
     <p>
-      This challenge is very simple, it consists of <b>coding at least an hour a day for 100 days</b> in a row. In parallel, the rule requires to post a tweet every day to indicate the work done and to keep track. I slightly modified this point, I will publish every day my progress on this small site to keep track of the entirety of my work without relying on Twitter. I will still publish my site on twitter from time to time to have feedback and reviews on what I do.
-      <br><br> If you want to learn more about this cool challenge, just visit the
+      This challenge is very simple, it consists of
+      <b>coding at least an hour a day for 100 days</b> in a row. In parallel, the rule requires to post a tweet every day to indicate the work done and to keep track. I slightly modified this point, I will publish every day my progress on this small site to keep track of the entirety of my work without relying on Twitter. I will still publish my site from time to time to have feedback and reviews on what I do.
+      <br>
+      <br>If you want to learn more about this cool challenge, just visit the
       <a
         href="https://www.100daysofcode.com/"
         target="_blank"
@@ -13,7 +15,7 @@
 </template>
 
 <style scoped>
-  a {
-    text-decoration: underline;
-  }
+a {
+  text-decoration: underline;
+}
 </style>
