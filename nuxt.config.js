18e8061cb2b7ff580d46d63549044c78099ff677
amerre <adr.merre@gmail.com>
2019-02-04 10:44:40 +0100

    :recycle: Delete fontawesome useless import

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,13 +19,6 @@ module.exports = {
         rel: "stylesheet",
         href:
           "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700"
-      },
-      {
-        rel: "stylesheet",
-        href: "https://use.fontawesome.com/releases/v5.7.0/css/all.css",
-        integrity:
-          "sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ",
-        crossorigin: "anonymous"
       }
     ]
   },
